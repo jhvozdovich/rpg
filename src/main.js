9be279d9d29bf4b1b2b998b3e66e0fbcbc2b750f
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-24 15:38:25 -0700

    add mana stat and ability array

--- src/main.js
+++ src/main.js
@@ -6,18 +6,17 @@ import $ from "jquery";
 
 const givePlayerName = changeState("name");
 const givePlayerType = changeState("type");
+const givePlayerAbility = changeState("ability");
 const givePlayerHealth = changeState("health");
 const givePlayerExperience = changeState("experience");
 const givePlayerLevel = changeState("level");
-
-
+const givePlayerMana = changeState("mana");
 $(document).ready(function () {
 
   $("#player-form").submit(function (event) {
     event.preventDefault();
 
     const player1 = storeState();
-    const player2 = storeState();
 
     var nameInput = $("#player-name").val();
     const player1Name = givePlayerName(nameInput);
@@ -25,21 +24,23 @@ $(document).ready(function () {
     var typeInput = $("#player-type").val();
     const playerType = givePlayerType(typeInput);
 
+    var abilityInput = $("#player-ability").val();
+    let abilityArr = [];
+    abilityArr.push("Punch");
+    abilityArr.push(abilityInput);
+    const playerAbility = givePlayerAbility(abilityArr);
+
 
     player1(player1Name);
     player1(playerType);
+    player1(playerAbility);
     player1(givePlayerExperience(0));
     player1(givePlayerHealth(10));
+    player1(givePlayerMana(5));
 
     const player1State = player1(givePlayerLevel(1));
 
-    const player2Name = givePlayerName("Emma");
-    const player2State = player2(player2Name);
-
-    // console.log(playerInfo(player1));
-    $("#output").text(`Name: ${player1State.name} PLAYER2NAME: ${player2State.name} Class: ${player1State.type} Level: ${player1State.level} Health: ${player1State.health} Experience: ${player1State.experience}`);
+    $("#output").text(`Name: ${player1State.name} Class: ${player1State.type} Ability: ${player1State.ability} Mana: ${player1State.mana} Level: ${player1State.level} Health: ${player1State.health} Experience: ${player1State.experience}`);
 
   });
 });
-
-//form name, character type, ability
